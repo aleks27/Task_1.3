92afd0a5d88c7dcfeafc189a1b29b8ac95519bf9
Aleks Nechayev <aleks.nechayev@gmail.com>
2016-12-14 22:50:15 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 4, d = 8;
+        System.out.println("With new line: " + a + " " + b + " " + c + " " + d);
+        System.out.print("Without new line: " + a + " " + b + " " + c + " " + d);
+    }
+}
